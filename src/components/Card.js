cc5d8e6c9d719f45ac156711af53097076821199
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-01 18:23:11 -0500

    Add styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ReactComponent as HeartsSVG } from 'images/hearts.svg';
 import { ReactComponent as ClubsSVG } from 'images/clubs.svg';
 import { ReactComponent as SpadesSVG } from 'images/spades.svg';
 import { ReactComponent as DiamondsSVG } from 'images/diamonds.svg';
+import { addHslAlpha } from 'styles/helpers';
+import { getRankAndSuit } from 'poker';
+
+// https://3dtransforms.desandro.com/card-flip
 
 const faces = {
-  '1': 'A',
+  '14': 'A',
   '11': 'J',
   '12': 'Q',
   '13': 'K'
@@ -19,20 +23,26 @@ const suitImageMap = {
   C: <ClubsSVG />
 };
 
+const heldStyles = css`
+  box-shadow: 0 0 0 0.5rem ${p => addHslAlpha(p.theme.colors.green, 1)};
+`;
+
 const Styles = styled.div`
+  --card-size: 8rem;
   position: relative;
-  width: 8rem;
-  height: 12rem;
+  width: var(--card-size);
+  height: calc(var(--card-size) * 1.4);
   padding: 1rem 0.75rem;
   font-family: Montserrat;
   line-height: 1;
-  font-size: 2.5rem;
+  font-size: calc(var(--card-size) * 0.3);
   color: ${p => ('HD'.includes(p.suit) ? p.theme.colors.red : p.theme.colors.offBlack)};
   border-radius: 0.5rem;
   border: 1px solid #ccc;
   background: white;
+  box-shadow: 0.2rem 0.2rem 1.5rem rgba(0, 0, 0, 0.2);
   svg {
-    --w: 3rem;
+    --w: calc(var(--card-size) * 0.35);
     width: auto;
     height: var(--w);
     position: absolute;
@@ -44,17 +54,18 @@ const Styles = styled.div`
     bottom: 1rem;
     right: 0.75rem;
   }
+  ${p => p.held && heldStyles}
 `;
 
-function Card({ value }) {
-  let ordinal = value.slice(0, -1);
-  ordinal = ordinal in faces ? faces[ordinal] : ordinal;
-  const suit = value.slice(-1);
+function Card({ value, held, onClick }) {
+  let [rank, suit] = getRankAndSuit(value);
+  rank = rank in faces ? faces[rank] : rank;
+
   return (
-    <Styles suit={suit}>
-      <span>{ordinal}</span>
+    <Styles suit={suit} held={held} onClick={onClick}>
+      <span>{rank}</span>
       {suitImageMap[suit]}
-      <span>{ordinal}</span>
+      <span>{rank}</span>
     </Styles>
   );
 }
