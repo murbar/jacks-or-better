d89f8a1347efe6c521df7e6992022295534c783a
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-03 01:06:20 -0500

    Add pulse/glow effect

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const shadow = '0 0 1rem rgba(0, 0, 0, 0.5)';
+
+const pulseKeyframes = keyframes`
+  from {  box-shadow: ${shadow}; }
+  to { box-shadow: 0 0 1.5rem yellow; }
+`;
+
+const pulse = css`
+  animation: ${pulseKeyframes} 0.75s alternate infinite;
+`;
 
 export default styled.button`
   font-size: 1em;
@@ -8,10 +19,13 @@ export default styled.button`
   background: ${p => p.theme.colors.offWhite};
   border-radius: 0.25rem;
   margin: 1rem 1rem 1rem 0;
+  box-shadow: ${shadow};
   &:last-child {
     margin-right: 0;
   }
   &:hover {
     cursor: pointer;
   }
+
+  ${p => p.pulse && pulse}
 `;
