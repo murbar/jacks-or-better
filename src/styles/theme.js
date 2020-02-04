8e6af8b986d351681d61bacadde2f52fb8a3406e
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-04 12:05:21 -0500

    Change colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
 const bgGradients = {
   blue:
     'radial-gradient(circle at 50% 30%, hsl(213, 80%, 37%) 0%, hsl(212, 85%, 23%) 100%)',
-  red: 'radial-gradient(circle at 50% 30%, hsl(13, 91%, 17%) 0%, hsl(15, 80%, 8%) 100%)',
+  red: 'radial-gradient(circle at 50% 30%, hsl(13, 91%, 22%) 0%, hsl(15, 80%, 10%) 100%)',
   green:
     'radial-gradient(circle at 50% 30%, hsl(150, 100%, 24%)  0%, hsl(152, 94%, 7%) 100%)'
 };
@@ -10,7 +10,7 @@ const colors = {
   offWhite: 'hsl(0, 0%, 94%)',
   offBlack: 'hsl(0, 0%, 13%)',
   green: 'hsl(144, 54%, 50%)',
-  gold: 'hsl(51, 100%, 50%)',
+  gold: 'hsl(48, 100%, 50%)',
   goldDark: 'hsl(42, 80%, 50%)',
   red: 'hsl(0, 100%, 38%)',
   blue: 'hsl(213, 80%, 37%)'
