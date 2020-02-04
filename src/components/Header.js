f2bdfdf1e487279331607ff4d0ab9238f1d13543
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-04 15:10:41 -0500

    Absolutely position header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 
 const Styles = styled.header`
   color: #eee;
+  position: absolute;
+  width: 50%;
+  top: 1rem;
+  right: 25%;
+  font-size: 1rem;
+  text-align: center;
 `;
 
 export default function Header() {
