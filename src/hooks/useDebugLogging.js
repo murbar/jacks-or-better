2f1ff0ba711329dcd237ebaeedc21b7a2420da99
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-09 18:39:32 -0500

    Remove unused hook

--- src/hooks/useDebugLogging.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-const envIsDevelopment = process.env.NODE_ENV === 'development';
-
-export default function useDebugLogging(data, label = 'Data') {
-  useEffect(() => {
-    if (envIsDevelopment) console.log(label, data);
-  }, [data, label]);
-}
