3cf386bfd5ea36a0ce84ee17aac487fb8983b789
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-17 15:01:44 -0500

    Add router context

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,21 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '__mocks__/AudioContext.mock';
 import '__mocks__/crypto.mock';
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import defaultTheme from 'styles/theme';
 
 import App from '../App';
 
-export const withTheme = (Component, props) => {
+export const withContexts = (Component, props) => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Component {...props} />
-    </ThemeProvider>
+    <MemoryRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Component {...props} />
+      </ThemeProvider>
+    </MemoryRouter>
   );
 };
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(withTheme(App), div);
+  ReactDOM.render(withContexts(App), div);
   ReactDOM.unmountComponentAtNode(div);
 });
